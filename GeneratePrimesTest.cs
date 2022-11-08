d788ee0994ab4628a503cbbeae3df65c02d0ea3a
ydkot735 <108911202+ydkot735@users.noreply.github.com>
2022-11-08 15:19:24 +0700

    Add files via upload

---
+++ GeneratePrimesTest.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+[TestFixture]
+public class GeneratePrimesTest
+{
+    [Test]
+    public void TestPrimes()
+    {
+        int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+        Assert.AreEqual(nullArray.Length, 0);
+        int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+        Assert.AreEqual(minArray.Length, 1);
+        Assert.AreEqual(minArray[0], 2);
+        int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+        Assert.AreEqual(threeArray.Length, 2);
+        Assert.AreEqual(threeArray[0], 2);
+        Assert.AreEqual(threeArray[1], 3);
+        int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+        Assert.AreEqual(centArray.Length, 25);
+        Assert.AreEqual(centArray[24], 97);
+    }
+}
